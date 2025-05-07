92a1a998b7115afc63495f619a23650429c1c7c1
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-05-07 13:13:12 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ain Jeong
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * This function does math by using given formula
+ */
+// eslint-disable-next-line no-unused-vars
+function doMath() {
+  let answer = 0
+
+  // input
+  const userNumber = parseFloat(document.getElementById('user-number').value)
+
+  // process
+  for (let counter = 1; counter <= userNumber; counter++) {
+    answer += counter
+  }
+
+  // output
+  document.getElementById('answer').innerHTML = 'Your answer is: ' + answer
+}
